6f4bc9b87f92d926311a02baab0d1706fc0e95ea
brkunver <burakhanunver@gmail.com>
2025-05-06 19:37:39 +0300

    create file

---
+++ utils/types.ts
@@ -0,0 +1,22 @@
+export const webSearchEngines = {
+    google: {
+        name: "Google",
+        symbol: "!g",
+        url: "https://www.google.com/search?q={query}",
+    },
+    duckduckgo: {
+        name: "DuckDuckGo",
+        symbol: "!d",
+        url: "https://www.duckduckgo.com/?q={query}",
+    },
+    bing: {
+        name: "Bing",
+        symbol: "!b",
+        url: "https://www.bing.com/search?q={query}",
+    },
+    youtube: {
+        name: "YouTube",
+        symbol: "!yt",
+        url: "https://www.youtube.com/results?search_query={query}",
+    },
+}
