d7e7596ed05fd2fbbb97530c86f89360daddc5c5
brkunver <burakhanunver@gmail.com>
2025-05-06 20:02:32 +0300

    add command listener

--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -2,4 +2,14 @@ export default defineBackground(() => {
   browser.runtime.onInstalled.addListener(() => {
     console.log("Extension installed")
   })
+
+  browser.commands.onCommand.addListener(command => {
+    if (command === "toggle-search") {
+      browser.tabs.query({ active: true, currentWindow: true }, tabs => {
+        if (tabs[0]) {
+          browser.tabs.sendMessage(tabs[0].id!, { action: "toggleSearchBar" })
+        }
+      })
+    }
+  })
 })
