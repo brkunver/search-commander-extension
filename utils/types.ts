78e88972a11271cc3d8b3bdd497fed59c9f30dc9
brkunver <burakhanunver@gmail.com>
2025-05-07 13:52:02 +0300

    add search engine names

--- utils/types.ts
+++ utils/types.ts
@@ -1,22 +1,23 @@
+export type searchEngine = "Google" | "DuckDuckGo" | "Bing" | "YouTube"
 export const webSearchEngines = {
-    google: {
-        name: "Google",
-        symbol: "!g",
-        url: "https://www.google.com/search?q={query}",
-    },
-    duckduckgo: {
-        name: "DuckDuckGo",
-        symbol: "!d",
-        url: "https://www.duckduckgo.com/?q={query}",
-    },
-    bing: {
-        name: "Bing",
-        symbol: "!b",
-        url: "https://www.bing.com/search?q={query}",
-    },
-    youtube: {
-        name: "YouTube",
-        symbol: "!yt",
-        url: "https://www.youtube.com/results?search_query={query}",
-    },
+  google: {
+    name: "Google",
+    symbol: "!g",
+    url: "https://www.google.com/search?q={query}",
+  },
+  duckduckgo: {
+    name: "DuckDuckGo",
+    symbol: "!d",
+    url: "https://www.duckduckgo.com/?q={query}",
+  },
+  bing: {
+    name: "Bing",
+    symbol: "!b",
+    url: "https://www.bing.com/search?q={query}",
+  },
+  youtube: {
+    name: "YouTube",
+    symbol: "!yt",
+    url: "https://www.youtube.com/results?search_query={query}",
+  },
 }
