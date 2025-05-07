1d011ce68b68568db8a569bddd344fee99905fda
brkunver <burakhanunver@gmail.com>
2025-05-07 15:16:04 +0300

    update

--- entrypoints/popup/App.tsx
+++ entrypoints/popup/App.tsx
@@ -1,9 +1,11 @@
 function App() {
   return (
-    <>
-      <h1>Search Commander</h1>
-      <p>Search the web using a keyboard shortcut</p>
-    </>
+    <div className="min-w-[300px] p-4 flex flex-col gap-2 items-center">
+      <h1 className="text-2xl font-bold">Search Commander</h1>
+      <p className="text-lg">
+        Press <kbd className="font-bold">Alt + S</kbd> to open the search bar
+      </p>
+    </div>
   )
 }
 
