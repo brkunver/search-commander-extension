7a391c31bc95ca6d1ffd8478a6d38d95de3ecd4c
brkunver <burakhanunver@gmail.com>
2025-05-08 15:00:43 +0300

    create logo

---
+++ assets/logos/yandex-logo.tsx
@@ -0,0 +1,19 @@
+import type { SVGProps } from "react"
+const Yandex = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    width="1em"
+    height="1em"
+    viewBox="0 0 256 256"
+    xmlns="http://www.w3.org/2000/svg"
+    preserveAspectRatio="xMidYMid"
+    {...props}
+  >
+    <path
+      d="M128 24c-12.15 0-22 9.85-22 22v60.5l-47.5 93.5c-7.5 14.5 2.5 32 19.5 32 10.5 0 17.5-6 22-14l27-54.5 27 54.5c4.5 8 11.5 14 22 14 17 0 27-17.5 19.5-32L150 106.5V46c0-12.15-9.85-22-22-22z"
+      fill="#E3122B"
+      transform="scale(1, -1) translate(0, -256)"
+    />
+
+  </svg>
+)
+export default Yandex
