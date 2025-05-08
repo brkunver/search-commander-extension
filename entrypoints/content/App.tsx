f5685a40bbfd8e9229d969c3d459034df2573310
brkunver <burakhanunver@gmail.com>
2025-05-08 15:01:32 +0300

    add translations, add logos

--- entrypoints/content/App.tsx
+++ entrypoints/content/App.tsx
@@ -3,12 +3,15 @@ import type { searchEngine } from "@/utils/types"
 import { engineSelector } from "@/utils/engine-selector"
 import { webSearchEngines } from "@/utils/types"
 import { replaceQuery } from "@/utils/replace-query"
+import { i18n } from "#imports"
+import useEngineLogo from "@/utils/use-logo"
 
 export default function App() {
   const [isActive, setIsActive] = useState(false)
   const [searchTerm, setSearchTerm] = useState("")
 
   let searchEngine: searchEngine = engineSelector(searchTerm) || "Google"
+  const Logo = useEngineLogo(searchEngine)
 
   function toggleSearchBar() {
     setIsActive(!isActive)
@@ -44,6 +47,7 @@ export default function App() {
         const searchWords = searchTerm.trim().split(" ")
         const isCommand = searchWords[0].startsWith("!")
         const query = isCommand ? searchWords.slice(1).join(" ") : searchTerm
+
         const url = replaceQuery(webSearchEngines[lowerCaseSearchEngine].url, query)
         window.open(url, "_blank")
         setSearchTerm("")
@@ -61,25 +65,30 @@ export default function App() {
   return (
     <div
       id="search-commander"
-      className="font-roboto fixed inset-0 flex items-center justify-center text-white"
-      style={{ zIndex: 2147483647, position: "fixed", top: 0, left: 0, width: "100vw", height: "100vh" }}
+      className="font-roboto fixed inset-0 flex items-start justify-center text-white z-[2147483647] pt-[33vh]"
     >
-      <div className="absolute inset-0 bg-black/90" style={{ zIndex: 2147483646 }} onClick={() => setIsActive(false)} />
-      <div className="relative w-full flex flex-col items-center gap-4" style={{ zIndex: 2147483647 }}>
-        <h1 className="text-2xl font-semibold">
-          Search on <span className="font-bold">{searchEngine}</span>
-        </h1>
-        <input
-          type="text"
-          autoFocus
-          placeholder="!g search..."
-          className="border-4 font-semibold text-lg border-white rounded-full py-2 px-4 w-full lg:w-1/2 focus:outline-none"
-          value={searchTerm}
-          onChange={e => setSearchTerm(e.target.value)}
-        />
+      <div
+        className="absolute inset-0 bg-black/40 backdrop-blur-md transition-all duration-200"
+        onClick={() => setIsActive(false)}
+      />
+      <div className="relative w-full flex flex-col items-center gap-4 z-[2147483647]">
+        <div
+          id="search-commander-input"
+          className="flex items-center gap-2 border antialiased border-white/30 rounded-2xl px-4 py-2 shadow mx-4 lg:mx-auto w-full lg:w-1/2 bg-neutral-900/80"
+        >
+          {Logo}
+          <input
+            type="text"
+            autoFocus
+            placeholder="!g search..."
+            className="font-semibold text-2xl py-2 px-2 w-full focus:outline-none placeholder-white/70 text-white"
+            value={searchTerm}
+            onChange={e => setSearchTerm(e.target.value)}
+          />
+        </div>
       </div>
-      <div className="fixed right-6 bottom-4 text-base px-3 py-1 rounded shadow-lg pointer-events-none select-none z-[2147483648]">
-        Press <kbd className="font-bold">Enter</kbd> to search, <kbd className="font-bold">Esc</kbd> to close
+      <div className="fixed right-6 bottom-4 text-base px-3 py-1 rounded shadow-lg pointer-events-none select-none z-[2147483648] bg-black/60 backdrop-blur text-white">
+        {i18n.t("content.info")}
       </div>
     </div>
   )
