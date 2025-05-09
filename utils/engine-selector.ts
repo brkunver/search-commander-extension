3e57f163138110a785e5f71ad691745388d85ab4
brkunver <burakhanunver@gmail.com>
2025-05-09 18:22:27 +0300

    enhance selection method

--- utils/engine-selector.ts
+++ utils/engine-selector.ts
@@ -2,6 +2,9 @@ import { searchEngine, webSearchEngines } from "@/utils/types"
 
 export default function engineSelector(searchTerm: string): searchEngine {
   const lowerTerm = searchTerm.toLowerCase()
-  const engine = webSearchEngines.find(engine => lowerTerm.startsWith(engine.symbol))
+  const parts = lowerTerm.split(" ")
+  const command = parts[0]
+
+  const engine = webSearchEngines.find(engine => engine.symbol === command)
   return engine ? engine.name : "Google"
 }
