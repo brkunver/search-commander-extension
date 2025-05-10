3155aec0fb2c0ab8ba3f0d2429c1340882a9b35e
brkunver <burakhanunver@gmail.com>
2025-05-10 15:18:22 +0300

    add open options

--- entrypoints/popup/App.tsx
+++ entrypoints/popup/App.tsx
@@ -1,19 +1,19 @@
+import { i18n } from "#imports"
+
 function App() {
-  function openHelp() {
-    browser.tabs.create({ url: browser.runtime.getURL("/help.html") })
+  function openOptions() {
+    browser.runtime.openOptionsPage()
   }
 
   return (
     <div className="min-w-[300px] p-4 flex flex-col gap-2 items-center">
-      <h1 className="text-2xl font-bold">Search Commander</h1>
-      <p className="text-lg">
-        Press <kbd className="font-bold">Alt + S</kbd> to open the search bar
-      </p>
+      <h1 className="text-2xl font-bold">{i18n.t("extName")}</h1>
+      <p className="text-lg">{i18n.t("popup.hint", ["Alt + s"])}</p>
       <button
-        onClick={openHelp}
+        onClick={openOptions}
         className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition-colors"
       >
-        Help
+        {i18n.t("popup.options")}
       </button>
     </div>
   )
